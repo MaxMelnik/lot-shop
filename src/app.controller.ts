8c7074d5ffd20accac9000aa2e0201a397987396
Maks Melnyk <maks.melnik1611@gmail.com>
2024-07-28 20:39:29 +0300

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import Redis from 'ioredis';
+import { Controller, Get } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+
+@Controller()
+export class AppController {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  @Get()
+  async getHello() {
+    await this.redis.set('key', 'Redis data!');
+    return await this.redis.get('key');
+  }
+}
